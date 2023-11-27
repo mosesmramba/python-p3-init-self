e958edaa8bde8d80fe52aa8c16e86f621fd0d2e9
mosesmramba <moses.mramba@student.moringaschool.com>
2023-11-27 12:35:37 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self,Name,Breed='Mutt'):
+        self.name=Name
+        self.breed=Breed
+
+# fido=Dog("Fido")
+# print(fido.breed)
+# print(fido.name)
